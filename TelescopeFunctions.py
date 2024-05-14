1eb8989a03eca1371753aafa30d23dd9fb3ae151
Francois <126506333+Francois0203@users.noreply.github.com>
2024-05-14 19:28:43 +0200

    Add files via upload

---
+++ TelescopeFunctions.py
@@ -0,0 +1,28 @@
+# Gui libraries
+import tkinter as tk 
+from tkinter import filedialog 
+
+# Select a save location
+def get_save_location():
+    root = tk.Tk()
+    root.withdraw()  # Hide the main tkinter window
+
+    save_location = filedialog.asksaveasfilename(defaultextension = ".txt")
+
+    # If the user cancels, save_location will be an empty string
+    if not save_location:
+        return None  
+
+    return save_location
+
+def __main__():
+    # Select save location
+    file_to_save = get_save_location()
+
+    if file_to_save != None:
+        with open(file_to_save, 'w') as f:
+            f.write("This is the content to save\n")
+
+        print("File saved to:", file_to_save)
+
+__main__()
