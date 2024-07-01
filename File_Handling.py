acc2ecb4eac2dbcbce6c57e0742e73077fb5d44d
Francois <francoismeiring0203@gmail.com>
2024-07-01 13:12:34 +0200

    Update File_Handling.py
    Added function to append to an existing file.

--- File_Handling.py
+++ File_Handling.py
@@ -39,10 +39,31 @@ def is_valid_directory(dir):
     return True, None  # No error message since the directory is valid
 
 def write_log():
+    record = ""
     time = datetime.now().strftime("%H:%M:%S")
     date = datetime.now().date()
 
+    record = str(date) + "\t" + time
+
+    return record
+
+def append_to_file(file_path, content): # content - String variable 
+    try:
+        with open(file_path, "a") as file: # Open the file in write mode ('a'). This will append to existing content.
+            file.write(content)
+        print("Successfully written to file:", file_path) 
+
+    except FileNotFoundError: # Handles the case where the file path is incorrect or inaccessible
+        print("Failed to load file:", file_path) 
+
+    except PermissionError: # Handles the case where the user doesn't have permission to write to the file
+        print("Permission denied. Cannot write to:", file_path)
+
+    except Exception as e: # A general exception handler for other unexpected errors
+        print(f"An error occurred: {e}")
+
 def __main__():
-    pass
+    print(write_log())
+
 if __name__ == '__main__':
     __main__()
