429442e9676973e2ad24d805629165d929a9d858
Francois <francoismeiring0203@gmail.com>
2024-08-17 17:56:15 +0200

    Update Main_Program.py
    Added new commands and descriptions. Some have functionality and some do not. Add log files to all commands.

--- Main_Program.py
+++ Main_Program.py
@@ -3,69 +3,58 @@ import os, string
 # Custom libraries
 import Calculations as C, File_Handling as FH, Telescope_Movement as TM
 
-USER, PASSWORD = '', ''
+USER, PASSWORD = 'f', 'f'
 
 COMMAND_DESCRIPTIONS = {
-        "telescope_control": "Control telescope movement and position.",
-        "data_file_management": "Manage data and files related to observations and telescope operations.",
-        "coordinate_systems": "Work with different coordinate systems for astronomical objects.",
-        "change_directory": "Change the current working directory.",
-        "display_logs": "Display system logs and records.",
-        "exit": "Terminate the program.",
-        "altaz_control": "Point the telescope at a specific point by using AltAz degrees.",
-        "radec_control": "Point the telescope at a specific point by using Ra and Dec values.",
-        "tracking": "Continuously adjust the telescope's position to follow a celestial object.",
-        "rest_mode": "Park the telescope in a safe position.",
-        "reporting": "Generate reports based on collected data.",
-        "file_path": "Specify the location for saving data files.",
-        "set_location": "Define the telescope's geographic location.",
-        "celestial_to_altaz_conversion": "Convert from celestial coordinates (ra and dec) to altaz degrees.",
-        "altaz_to_celestial_conversion": "Convert from alt az degrees to celestial coordinates (ra and dec)."
-    }
-
-COMMAND_DESCRIPTIONS = {
-    "": "",
-    "": ""
+    "Telescope Control": "Display menu responsible for telescope control functions. ",
+    "Configure Settings": "Display menu responsible for configuration settings. ",
+    "Coordinate System": "Display menu responsible for coordinate calculations and conversions. ",
+    "Display Data": "Display menu responsible for displaying system info. ",
+    "Exit": "Exit the RTOS program. ",
+    "Point to AltAz": "Point telescope to specific Alt (altitude) & Az (azimuth) degrees. ",
+    "Point To RaDec": "Point telescope to specific Ra (right ascension) & Dec (declination) values. ",
+    "Tracking": "Initiate tracking process to track a celestial object. ",
+    "Rest Mode": "Move telescope to rest mode. ",
+    "Back": "Go back to the previous menu. ",
+    "Change Telescope Location": "Change physical location values of telescope (Latitude, Longitude, Elevation). ",
+    "Change Data Location": "Change the location where the telescope frequency data is stored. ",
+    "Convert Alt & Az to Ra & Dec": "Convert Alt (altitude) & Az (azimuth) degrees to Ra (right ascension) & Dec (declination) values.",
+    "Convert Ra & Dec to Alt & Az": "Convert Ra (right ascension) & Dec (declination) values to Alt (altitude) & Az (azimuth) degrees.",
+    "Display location": "Display location using IP, GPS, and the last stored location in the configuration file. ",
+    "Display Telescope Logs": "Display log files created by software. ",
+    "Display All Commands & Descriptions": "Display list of all commands in program and their descriptions. "
 }
 
 MAIN_MENU = ["1. Telescope Control", 
             "2. Configure Settings",
-            "3. Coordinate Systems",
+            "3. Coordinate System",
             "4. Display Data",
             "5. Exit"]
 
 TELESCOPE_CONTROL_MENU = ["1. Point To AltAz",
                         "2. Point To RaDec",
                         "3. Tracking",
-                        "4. Rest Mode"]
+                        "4. Rest Mode",
+                        "5. back"]
 
-CONFIG_SETTINGS_MENU = ["1. Change Location",
-                        "2. Change Location where Data is Stored"]
+CONFIG_SETTINGS_MENU = ["1. Change Telescope Location",
+                        "2. Change Data Location",
+                        "3. back"]
 
-COORDINATE_MENU =["1. Convert Alt & Az to Ra & Dec",
-                "2. Convert Ra & Dec to Alt & Az"]
+COORDINATE_MENU = ["1. Convert Alt & Az to Ra & Dec",
+                "2. Convert Ra & Dec to Alt & Az",
+                "3. back"]
 
 DISPLAY_SYS_DATA_MENU = ["1. Display Location",
                         "2. Display Telescope Logs",
-                        "3. Display All Commands & Descriptions"]
-
-# def display_menu():
-#     print("\n")
-
-#     print("*******************************")
-#     print("   Radio Telescope Control     ")
-#     print("*******************************")
-#     print("1. Telescope Control")
-#     print("2. Data Management")
-#     print("3. Coordinate Systems")
-#     print("4. cd")
-#     print("5. Display Logs")
-#     print("6. Exit")
-
-#     print("\n")
+                        "3. Display All Commands & Descriptions",
+                        "4. back"]
 
 def display_menu(menu_num):
+    print("\n")
+
     if menu_num == 0: # Main Menu
+        print("\n")
         print("*******************************")
         print("   Radio Telescope Control     ")
         print("*******************************")
@@ -85,161 +74,111 @@ def display_menu(menu_num):
         for i in DISPLAY_SYS_DATA_MENU:
             print(i)
 
-def display_COMMAND_DESCRIPTIONS(COMMAND_DESCRIPTIONS):
-    for key, value in COMMAND_DESCRIPTIONS.items():
-        print(f"{key}: {value}")
+def telescope_control_functions(choice):
+    if choice == 1: # Point to altaz 
+        alt = float(input("Enter altitude degrees: "))
+        az = float(input("Enter azimuth degrees: "))
+
+        # ADD FUNCTIONALITY TO MOVE TELESCOPE HERE
+    elif choice == 2: # Point to radec
+        ra = float(input("Enter Ra value: "))
+        dec = float(input("Enter dec value"))
+        C.convert_radec_to_altaz(ra, dec)
+
+        # ADD FUNCTIONALITY TO MOVE TELESCOPE HERE
+    elif choice == 3: # Tracking
+        code = input("\nEnter the code of the celestial object that you would like to track: ")
+        print("\n")
+
+        TM.track_celestial_object(code)
+    elif choice == 4: # Rest mode
+        # ADD FUNCTIONALITY TO MAKE TELESCOPE ENTER REST MODE
+        pass
+    elif choice == 5: # Back to main menu, do not check this value
+        pass
+    else:
+        print("Invalid input, please enter a number next to the command you want to execute.")
+
+def configure_settings_functions(choice):
+    if choice == 1: # Change Telescope Location
+        pass
+    elif choice == 2: # Data store location
+        pass
+    elif choice == 3: # Back to main menu, do not check this value
+        pass
+    else: print("Invalid input, please enter a number next to the command you want to execute.")
+
+def coordinate_functions(choice):
+    if choice == 1: # Convert AltAz to Ra & Dec
+        alt = float(input("Enter altitude degrees: "))
+        az = float(input("Enter azimuth degrees: "))
+        ra, dec = C.convert_altaz_to_radec(alt, az)
+
+        print(f"Altaz converted to ra and dec:  RA: {ra}  DEC: {dec}")
+    elif choice == 2: # Convert Ra & Dec to AltAZ
+        ra = float(input("Enter Ra value: "))
+        dec = float(input("Enter dec value"))
+        alt, az = C.convert_radec_to_altaz(ra, dec)
+
+        print(f"radec converted to alt and az:  ALT: {alt}  AZ: {az}")
+    elif choice == 3: # Back to main menu, do not check this value
+        pass
+    else: print("Invalid input, please enter a number next to the command you want to execute.")
+
+def display_sys_data_functions(choice):
+    if choice == 1: # Display location
+        print("(Latiude, Longitude, Elevation)")
+        print(f"IP: {C.ip_get_location_and_elevation()}")
+        # print(f"GPS: {}")
+        # print(f"Last Saved: {}")
+    elif choice == 2: # Display telescope logs
+        FH.display_logs()
+    elif choice == 3: # Display commands & descriptions
+        print("\nAvailable commands: \n")
+
+        for command, description in COMMAND_DESCRIPTIONS.items():
+            print(f"{command}: {description}")
+    elif choice == 4: # Back to main menu, do not check this value
+        pass
+    else: print("Invalid input, please enter a number next to the command you want to execute.")
 
 def __main__():
-    USER = input("Enter your username: ")
-    PASSWORD = input("Enter your password: ")
+    b_flag = False
+
+    # Check login details
+    while b_flag == False:
+        user = input("Enter your username: ")
+        password = input("Enter your password: ")
+
+        if (USER == user) & (PASSWORD == password):
+            b_flag = True
+        else: print("Incorrect login!!!\n")
+
     print("\n")
 
     while True:
         display_menu(0) # Display main menu
         choice = int(input("\nEnter your choice: ")) # Input to select menu
-
-        print("\n")
         display_menu(choice)
 
         if choice == 1: # Telescope control
-            pass
+            tc_choice = int(input("\nEnter your choice: "))
+            telescope_control_functions(tc_choice)
         elif choice == 2: # Configure settings
-            pass
+            cs_choice = int(input("\nEnter your choice: "))
+            configure_settings_functions(cs_choice)
         elif choice == 3: # Coordinate system
-            pass
+            c_choice = int(input("\nEnter your choice: "))
+            coordinate_functions(c_choice)
         elif choice == 4: # Display system data
-            pass
+            sd_choice = int(input("\nEnter your choice: "))
+            display_sys_data_functions(sd_choice)
         elif choice == 5: # Exit
             exit()
         else:
-            print("Invalid option, please enter the number next to the menu item.")
+            print("Invalid input, please enter a number next to the command you want to execute.")
         
         print("\n")
-
-def __test__():
-    user = input("Enter your username: ") # Get username for logging
-
-    while True:
-        display_menu() # Display the main menu
-        # Get user input for main menu choice
-        choice = input("Enter your choice: ")
-        print("\n")
-        if choice == '1':
-            print("Telescope Control Menu:")
-            print("1. Insert AltAz degrees")
-            print("2. Insert Ra and Dec values")
-            print("3. Tracking")
-            print("4. Rest Mode")
-            print("\n")
-            tc_choice = input("Enter your choice: ")
-            if tc_choice == "1":
-                print(COMMAND_DESCRIPTIONS["altaz_control"])
-
-                alt = int(input("Alt degrees (vertical control): "))
-                az = int(input("Az degrees (horisontal control): "))
-
-                # ADD CODE TO MOVE TELESCOPE
-
-                FH.write_log(user,"AltAz Control","AltAz control initiated")
-            elif tc_choice == "2":
-                print(COMMAND_DESCRIPTIONS["radec_control"])
-                
-                ra = int(input("Ra value (Right Ascension): "))
-                dec = int(input("Dec value (Declination): "))
-                
-                alt, az = C.convert_radec_to_altaz(ra, dec)
-
-                # ADD CODE TO MOVE TELESCOPE
-
-                FH.write_log(user, "Ra Dec Control", "Ra Dec Control initiated")
-            elif tc_choice == "3":
-                print(COMMAND_DESCRIPTIONS["tracking"])
-                
-                code = input("Insert the code of the object you would like to track: ")
-                TM.track_celestial_object(code)
-
-                FH.write_log(user, "Tracking", "Tracking initiated")
-            elif tc_choice == "4":
-                print(COMMAND_DESCRIPTIONS["rest_mode"])
-                
-                TM.telescope_rest()
-
-                FH.write_log(user, "Rest Mode", "Rest Mode initiated")
-            else:
-                print("Invalid choice")
-
-        elif choice == '2':
-            print("Data Management Menu:")
-            print("1. Reporting")
-            print("2. File Path")
-            print("\n")
-            dm_choice = input("Enter your choice: ")
-            if dm_choice == "1":
-                print(COMMAND_DESCRIPTIONS["reporting"])
-                # Replace with actual reporting logic
-                FH.write_log(user, "Reporting", "Report Generation initiated")
-            elif dm_choice == "2":
-                print(COMMAND_DESCRIPTIONS["file_path"])
-                FH.write_log(user, "File Path", "Changed the File Path ")
-                # Replace with actual file path setting logic
-            else:
-                print("Invalid choice")
-
-        elif choice == '3':
-            print("Coordinate Systems Menu:")
-            print("1. Set Location")
-            print("2. Convert Celestial Frame to AltAz Degrees")
-            print("3. Convert AltAz Degrees to Celestial Frame")
-            print("\n")
-            cs_choice = input("Enter your choice: ")
-            if cs_choice == "1":
-                print(COMMAND_DESCRIPTIONS["set_location"])
-                FH.write_log(user, "Set Location", "Defined the Geographic Location of the Telescope ")
-                # add set locations functionality
-            elif cs_choice == "2":
-                print(COMMAND_DESCRIPTIONS["celestial_to_altaz_conversion"], '\n')
-                ra = float(input("Enter the right ascesion (ra) value: "))
-                dec = float(input("Enter the declination (dec) value: "))
-
-                alt_deg, az_deg = C.celestial_to_altaz(ra, dec)
-
-                print(f"Current Location: {C.get_location_and_elevation()}")
-                print(f"Altitude: {alt_deg:.2f} degrees, Azimuth: {az_deg:.2f} degrees")
-                FH.write_log(user, "Convert Celestial Frame to AltAz Degrees", f"Celestial to Altaz Conversion: {ra}, {dec} -> {alt_deg:.2f}, {az_deg:.2f}")
-            elif cs_choice == "3":
-                print(COMMAND_DESCRIPTIONS["altaz_to_celestial_conversion"])
-                alt = float(input("Enter the altitude (alt) value: "))
-                az = float(input("Enter the azimuth (az) value: "))
-
-                ra, dec = C.altaz_to_celestial(alt, az)
-
-                print(f"Current Location: {C.get_location_and_elevation()}")
-                print(f"Celestial Coordinates (ra, dec): ({ra}, {dec})")
-                FH.write_log(user, "Convert AltAz Degrees to Celestial Frame", f"Altaz to Celestial Conversion: {alt}, {az} -> {ra:.2f}, {dec:.2f}")
-            else:
-                print("Invalid choice")
-
-        elif choice == '4':
-            print(COMMAND_DESCRIPTIONS["change_directory"])
-            directory = input("Enter new directory: ")
-            try:
-                os.chdir(directory)
-                FH.write_log(user, "Change Directory", f"Changed directory to: {os.getcwd()}")
-                print(f"Changed directory to: {os.getcwd()}")
-            except OSError:
-                print("Directory change failed.")
-                FH.write_log(user, "Change Directory", f"Directory change failed: {directory}")
-
-        elif choice == "5":
-            print(COMMAND_DESCRIPTIONS["display_logs"], '\n')
-            FH.display_logs()
-        elif choice == "6":
-            print(COMMAND_DESCRIPTIONS["exit"])
-            FH.write_log(user,"Exit","Terminated the program")
-            exit()
-        else:
-            print("Invalid choice. Please try again.")
-
 
 if __name__ == '__main__':
     __main__()
